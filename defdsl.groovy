bab2a2411d1ed8274d17328cf8301bca6ea7734e
Diogo Lucas <diogo.lucas@agcocorp.com>
2015-05-13 18:25:53 -0300

    Wiring complete.

--- defdsl.groovy
+++ defdsl.groovy
@@ -283,16 +283,21 @@ class SpecLoader {
 
     private def loadPath(Path pathSet) {
         pathSet.paths.each { path ->
-            def pathName = path.key
-            println "registering path $pathName"
-            verbs.each { verb ->
-                if (path.value[verb]) {
-                    println "registering verb ${pathName}.${verb}"
-                    spark.Spark."$verb"(pathName, path.value[verb].run)
-                }
+            loadPathSpec(path.value, path.key)
+        }
+    }
+
+    private def loadPathSpec(PathSpec path, String pathName) {
+        println "registering path $pathName"
+        verbs.each { verb ->
+            if (path[verb]) {
+                println "registering verb ${pathName}.${verb}"
+                spark.Spark."$verb"(pathName, path[verb].run)
             }
         }
-
+        path.children.each {
+            loadPathSpec(it.value, pathName + it.key)
+        }
     }
 }
 
