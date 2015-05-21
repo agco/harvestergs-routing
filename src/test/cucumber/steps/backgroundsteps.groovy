4f64f3c3a6b0eb84a999529da7c75e0a41bf9ba0
Diogo Lucas <diogo.lucas@agcocorp.com>
2015-05-21 13:47:03 -0300

    routing tests now passing

---
+++ src/test/cucumber/steps/backgroundsteps.groovy
@@ -0,0 +1,58 @@
+import cucumber.api.PendingException
+import groovy.json.JsonSlurper
+
+import static cucumber.api.groovy.EN.*
+import groovyx.net.http.RESTClient
+import groovyx.net.http.*
+import static groovyx.net.http.ContentType.JSON
+
+def resources = []
+
+Given(~/^a set of related resources$/) { ->
+    // Write code here that turns the phrase above into concrete actions
+    def commentResource = new Resource()
+    commentResource.definitions =
+            commentResource.definitions
+            .Comments {
+                properties {
+                    body {
+                        type 'string'
+                        description 'Comments contents'
+                    }
+                }
+                required 'body'
+            }
+
+    commentResource.paths =
+        commentResource.paths
+        ."/comments" {
+            get { req, res ->
+                "comments.get"
+            }
+
+            post { req, res ->
+                "comments.post"
+            }.document { docs ->
+                docs.description = "Description for comments.post"
+                docs
+            }
+            .skipAuth
+                    .skipValidation
+
+            "/:id" {
+                get    {req, res -> "comments/1.get"}
+                patch  {req, res -> "comments/1.patch"}
+                        .document { docs -> docs.operationId = "commentUpdate"; docs }
+                delete {req, res -> "comments/1.delete"}
+            }
+        }
+
+    resources << commentResource
+}
+
+Given(~/^these resources are loaded into an API$/) { ->
+    def loader = new ResourceLoader()
+    resources.each {
+        loader.loadResource it
+    }
+}
---
+++ src/test/cucumber/steps/loaderSteps.groovy
@@ -0,0 +1,98 @@
+import cucumber.api.PendingException
+import groovy.json.JsonSlurper
+
+import static cucumber.api.groovy.EN.*
+import groovyx.net.http.RESTClient
+import groovyx.net.http.*
+import static groovyx.net.http.ContentType.JSON
+
+
+def sut
+def input
+def target
+
+def client = new RESTClient('http://localhost:4567')
+def targets = [
+        "comments" : [
+                "get" : null,
+                "post": [ body: 'foobar' ]
+        ],
+        "comments/1": [
+                "get": null,
+                "patch": [ body: 'test'],
+                "delete": null
+        ]
+]
+
+Given(~/^the aforementioned resource definition$/) { ->
+    // no action needed here -- all the setup occurred in the background steps
+}
+
+
+def error
+
+When(~/^I post a resource that is missing mandatory fields$/) { ->
+    // Write code here that turns the phrase above into concrete actions
+    def resource = '{}'
+    try {
+        response = client.post(path: '/comments', requestContentType: ContentType.JSON)
+        fail("HTTP action should have returned an error")
+    }
+    catch(HttpResponseException e) {
+        error = e
+    }
+}
+
+Then(~/^I receive a (\d+) code$/) { code ->
+    assert error.statusCode.toString() == code
+}
+
+def msg
+
+Then(~/^the response is a valid jsonapi error$/) { ->
+    assert error.response.responseData
+    msg = error.response.responseData
+    assert msg.id
+    assert msg.title
+    assert msg.detail
+}
+
+Then(~/^the details list all missing fields$/) { ->
+    msg.detail.contains('name')
+}
+
+When(~/^I get the documentation for it$/) { ->
+    throw new PendingException()
+    response = client.post(path: '/swagger', requestContentType: ContentType.JSON)
+}
+
+Then(~/^I receive a swagger-compliant response$/) { ->
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException()
+}
+
+Then(~/^the response correctly describes the resource$/) { ->
+    // Write code here that turns the phrase above into concrete actions
+    throw new PendingException()
+}
+
+When(~/^I reach its REST endpoints$/) { ->
+    // ugly for now, but leaving all checks at the 'then' clause
+    // makes test a lot simpler. Refactor if this hurts.
+}
+
+def response
+def expectedBody
+When(~/^I run a (\w+) at path (\w+)$/) { verb, path ->
+    def body = targets[path][verb]
+    response = client."$verb"(path: path, requestContentType: ContentType.JSON, body: body)
+    expectedBody = "${path}.${verb}"
+}
+
+Then(~/^I receive a (\d+) response code$/) { int code ->
+    assert response.status == code
+}
+
+Then(~/^the response message is correct$/) { ->
+    assert response.responseData == expectedBody
+}
