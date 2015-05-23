ad323d4eeec6f283b6a71998bf377bcda2fbb9fc
Diogo Lucas <diogo.lucas@agcocorp.com>
2015-05-22 23:35:23 -0300

    Fixing syntax in one of the examples, removing some dummy code

--- src/main/groovy/ResourceLoader.groovy
+++ src/main/groovy/ResourceLoader.groovy
@@ -32,10 +32,12 @@ class ResourceLoader {
     }
 
     private def loadPathSpec(PathSpec path, String pathName) {
-        println "registering path $pathName"
+        //todo: turn this into log entries
+        //println "registering path $pathName"
         verbs.each { verb ->
             if (path[verb]) {
-                println "registering verb ${pathName}.${verb}"
+                //todo: turn this into log entries
+                //println "registering verb ${pathName}.${verb}"
                 spark.Spark."$verb" pathName, path[verb].run
             }
         }
