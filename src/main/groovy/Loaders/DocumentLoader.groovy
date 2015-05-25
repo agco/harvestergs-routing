9102022519480fd2f1cc337347c7db402d4b0761
Diogo Lucas <diogo.lucas@agcocorp.com>
2015-05-25 18:36:14 -0300

    Api template now expanded. Still missing cfg input and a test fix.

--- src/main/groovy/Loaders/DocumentLoader.groovy
+++ src/main/groovy/Loaders/DocumentLoader.groovy
@@ -1,21 +1,36 @@
 import groovy.json.JsonSlurper
+import groovy.text.SimpleTemplateEngine
 
 class DocumentLoader {
     private slurper = new JsonSlurper()
+    private specProperties
+    private engine = new SimpleTemplateEngine()
+    private templates = [:]
 
-    def DocumentLoader(PathVisitor pathVisitor = new PathVisitor()) {
+    def DocumentLoader(PathVisitor pathVisitor = new PathVisitor(),
+                       specProperties = ['host': 'localhost', 'version': '0.1', 'description': 'api description', 'title': 'api title']) {
         this.pathVisitor = pathVisitor
+        this.specProperties = specProperties
     }
 
-    private def loadSpec(specName) {
-        def spec = getClass().getResourceAsStream('templates/apiSpec.template.json')
-        slurper.parseText(spec.text)
+    private getTemplate(specName) {
+        def specRaw = getClass().getResourceAsStream("templates/${specName}Spec.template.json").text
+        if (!templates[specName]) {
+            templates[specName] = engine.createTemplate(specRaw)
+        }
+        templates[specName]
+    }
+
+    private def loadSpec(specName, props) {
+        def tpl = getTemplate(specName)
+        def spec = tpl.make(props).toString()
+        slurper.parseText(spec)
     }
 
     def PathVisitor pathVisitor
 
     def loadDocs(Resource spec) {
-        def root = loadSpec 'api'
+        def root = loadSpec 'api', specProperties
 
         def visitor = { path, pathName ->
             path.properties.each { prop, val ->
--- src/test/cucumber/steps/loaderSteps.groovy
+++ src/test/cucumber/steps/loaderSteps.groovy
@@ -87,8 +87,8 @@ Then(~/^I receive a swagger-compliant response$/) { ->
     // Write code here that turns the phrase above into concrete actions
     assert response
     assert response.responseData
-    assert response.responseData.swagger == "2.0"
-    assert response.responseData.info.title
+    //assert response.responseData.swagger == "2.0"
+    //assert response.responseData.info.title == "testApp"
     throw new PendingException()
 }
 
