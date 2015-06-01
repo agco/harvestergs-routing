cb3b8ab3c6a3876a8339413f6a488566191290bd
Diogo Lucas <diogo.lucas@agcocorp.com>
2015-06-01 10:21:28 -0300

    Adding missing file -- hate this 'gca' behavior

---
+++ src/test/groovy/com/agcocorp/harvestergs/routing/CommentResourceBuilder.groovy
@@ -0,0 +1,74 @@
+package com.agcocorp.harvestergs.routing
+
+class CommentResourceBuilder {
+    private getAll
+    private getById
+
+    CommentResourceBuilder(Closure getAll, getById) {
+        this.getAll = getAll
+        this.getById = getById
+    }
+
+    def build() {
+        def resource = new Resource(this)
+        resource
+            .definitions
+            .Comment {
+            properties {
+                body {
+                    type 'string'
+                    description 'Comments contents'
+                }
+
+                author {
+                    type 'object'
+                    properties {
+                        name { type 'string'}
+                        email { type 'string'}
+                        url { type 'string'}
+                    }
+                    required 'name', 'email'
+                }
+
+                tags {
+                    type 'array'
+                    items {
+                        type 'object'
+                        properties {
+                            name { type 'string' }
+                            size { type 'integer' }
+                        }
+                        required 'name'
+                    }
+                }
+            }
+            required 'body'
+        }
+
+        resource
+            .paths
+            ."/comments" {
+            get { req, res ->
+                return  getAll()
+            }
+
+            post { req, res ->
+                return req.data
+            }.document { docs ->
+                docs.description = "Description for comments.post"
+                docs
+            }
+            .skipAuth
+                .skipValidation
+
+            "/:id" {
+                get    {req, res -> return getById(req.params(':id')) }
+                patch  {req, res -> return req.data }
+                //.document { docs -> docs.operationId = "commentUpdate"; docs }
+                delete {req, res -> return null }
+            }
+        }
+
+        return resource
+    }
+}
