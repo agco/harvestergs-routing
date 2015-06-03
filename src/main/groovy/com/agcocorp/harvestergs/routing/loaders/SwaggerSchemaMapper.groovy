56e4472552a2be6532fefb6dfded52500a9c6f8b
Diogo Lucas <diogo.lucas@agcocorp.com>
2015-06-02 22:07:12 -0300

    Shallow nesting in. Still missing recursive nesting, arrays and other goodies...

--- src/main/groovy/com/agcocorp/harvestergs/routing/loaders/SwaggerSchemaMapper.groovy
+++ src/main/groovy/com/agcocorp/harvestergs/routing/loaders/SwaggerSchemaMapper.groovy
@@ -1,8 +1,50 @@
 package com.agcocorp.harvestergs.routing.loaders
 
 class SwaggerSchemaMapper {
+    private setInnerProp(obj, prop, value) {
+        def current = obj
+        def props = prop.tokenize('.')
+        def last = props.size() - 1
+
+        props.eachWithIndex {it, idx ->
+            if (idx == last) {
+                current[it] = value
+            }
+            else {
+                if (current[it] == null) {
+                    current[it] = [:]
+                }
+                current = current[it]
+            }
+        }
+    }
+
+    private setIfNotNull(obj, prop, value) {
+        if (value) {
+            setInnerProp obj, prop, value
+        }
+    }
+
+    private setNotNull(obj, prop, value) {
+        if (!value) {
+            // todo: proper exception throw here
+            throw new RuntimeException()
+        }
+        setInnerProp obj, prop, value
+    }
+
     def map(schema) {
-        def swagger = [ data: [:]]
+        def swagger = [ properties: [ data: [:] ] ]
+        def data = swagger.properties.data
+        setIfNotNull data, 'properties.type', schema.type
+        if (schema.attributes) {
+            setNotNull data, 'properties.attributes', [properties:[:]]
+            def attr = data.properties.attributes
+            attr.type = 'object'
+            schema.attributes.each {
+                attr.properties[it.key] = it.value
+            }
+        }
         return swagger
     }
 }
--- src/test/groovy/com/agcocorp/harvestergs/routing/SwaggerSchemaMapperTest.groovy
+++ src/test/groovy/com/agcocorp/harvestergs/routing/SwaggerSchemaMapperTest.groovy
@@ -1,15 +1,127 @@
 package com.agcocorp.harvestergs.routing
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.github.fge.jsonschema.main.JsonSchemaFactory
 import spock.lang.*
 import com.agcocorp.harvestergs.routing.loaders.SwaggerSchemaMapper
 
 class SwaggerSchemaMapperTest extends Specification {
-    def "test jsonapi schema to swagger mapping"(Map schema, swagger) {
+    def jsonSchemaFactory = JsonSchemaFactory.byDefault()
+    def objectMapper = new ObjectMapper()
+    final testData = [
+        'typed schema': [ type: 'comment' ],
+        'typed swagger': [ properties: [ data: [ properties: [ type: 'comment' ] ] ] ],
+        'simple schema': [ type: 'comment', attributes: [ body: [ type: 'string'] ] ],
+        'simple swagger': [ properties: [ data: [ properties: [ type: 'comment', attributes: [ type: 'object', properties: [ body: [type: 'string' ] ] ] ] ] ] ],
+        'nested schema': [
+            type: 'comment',
+            attributes: [
+                body: [ type: 'string'],
+                author: [
+                    type: 'object',
+                    properties: [
+                        name: [ type: 'string' ],
+                        email: [ type: 'string']
+                    ]
+                ]
+            ]
+        ],
+        'nested swagger': [
+            properties: [
+                data: [
+                    properties: [
+                        type: 'comment',
+                        attributes: [
+                            type: 'object',
+                            properties: [
+                                body: [ type: 'string' ],
+                                author: [
+                                    type:'object',
+                                    properties: [
+                                        name: [type: 'string'],
+                                        email: [type: 'string']
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ],/*
+        'deep schema': [
+            type: 'comment',
+            attributes: [
+                body: [ type: 'string'],
+                author: [
+                    type: 'object',
+                    properties: [
+                        name: [ type: 'string' ],
+                        email: [ type: 'string'],
+                        avatars: [
+                            // todo: allow for null prop type
+                            type: 'object',
+                            properties: [
+                                small: [ type: 'string'],
+                                medium: [ type: 'string'],
+                                large: [ type: 'string']
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ],
+        'deep swagger': [
+            properties: [
+                data: [
+                    properties: [
+                        type: 'comment',
+                        attributes: [
+                            type: 'object',
+                            properties: [
+                                body: [ type: 'string' ],
+                                author: [
+                                    type:'object',
+                                    properties: [
+                                        name: [type: 'string'],
+                                        email: [type: 'string'],
+                                        avatars: [
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ],*/
+        'empty schema': [ : ],
+        'empty swagger': [ properties: [ data: [ : ] ] ]
+    ]
+
+    def checkSwagger(swagger) {
+        def schema = jsonSchemaFactory.getJsonSchema("resource:/com/agcocorp/harvestergs/routing/swagger-schema.json")
+        def data = objectMapper.valueToTree(swagger)
+        def valResults = schema.validate(data)
+        assert valResults.isSuccess()
+    }
+
+    def testMatch(Map schema, swagger) {
+        def sut = new SwaggerSchemaMapper()
+        assert sut.map(schema) == swagger
+        true
+    }
+
+    def "jsonapi schema to swagger mapping"(String schema, swagger) {
         def sut = new SwaggerSchemaMapper()
         expect:
-            sut.map(schema) == swagger
+            testMatch(testData[schema], testData[swagger])
+            //checkSwagger(swagger)
 
         where:
-            schema                  |               swagger
-            [:]                     | [ data: [:] ]
+            schema              |      swagger
+            'empty schema'      |   'empty swagger'
+            'typed schema'      |   'typed swagger'
+            'simple schema'     |   'simple swagger'
+            'nested schema'     |   'nested swagger'
     }
 }
